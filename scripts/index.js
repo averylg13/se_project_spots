5416a3e29fe36162c8b79a4d3456d21512b128ea
averylg13 <averylg13@gmail.com>
2025-03-02 21:40:16 -0600

    updated html, fixed formatting, added alt tags to images via javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,80 +1,85 @@
 const initialCards = [
-    {
+  {
     name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg" , 
-        },
-    {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    alt: "an image of a snowy apartment building on a television",
+  },
+  {
     name: "Restaurant terrace",
-     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" ,
-        }, 
-    {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    alt: "a cozy outdoor restaurant",
+  },
+  {
     name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg" ,
-       },
-    {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    alt: "people sitting at small tables drinking coffee",
+  },
+  {
     name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg" ,
-       },
-    {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    alt: "a very long bridge, over a forrest",
+  },
+  {
     name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg" ,
-       },
-    {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    alt: "a tunnel with light shining through windows",
+  },
+  {
     name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" ,
-       },
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    alt: "a wooden house in a snowy forrest",
+  },
 ];
 
-
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__description');
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 const profileCloseButton = document.querySelector(".modal__close-btn");
 
+const editModal = document.querySelector("#edit-profile-modal");
+const editFormElement = editModal.querySelector(".modal__form");
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+const editModalDescriptionInput = editModal.querySelector(
+  "#profile-description-input"
+);
 
-const editModal = document.querySelector('#edit-profile-modal');
-const editFormElement = editModal.querySelector('.modal__form');
-const editModalNameInput = editModal.querySelector('#profile-name-input');
-const editModalDescriptionInput = editModal.querySelector('#profile-description-input');
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards");
 
-const cardTemplate = document.querySelector('#card-template');
-const cardsList = document.querySelector('.cards');
-
-
-function getCardElement (data) {
-   const cardElement = cardTemplate.content.querySelector('.card').cloneNode(true);
-   const cardName = cardElement.querySelector('.card__name');
-   const cardImage = cardElement.querySelector('.card__image');
-   cardName.textContent = data.name;
-   cardImage.src = data.link;
-   return cardElement;
-};
-
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+  const cardName = cardElement.querySelector(".card__name");
+  const cardImage = cardElement.querySelector(".card__image");
+  cardName.textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.alt;
+  return cardElement;
+}
 
 function openModal() {
-   editModalNameInput.value = profileName.textContent;
-   editModalDescriptionInput.value = profileDescription.textContent;
-editModal.classList.add("modal_opened");
-};
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
+  editModal.classList.add("modal_opened");
+}
 
 function closeModal() {
-   editModal.classList.remove("modal_opened");
-};
+  editModal.classList.remove("modal_opened");
+}
 
 function handleEditFormSubmit(evt) {
-   evt.preventDefault();
-   profileName.textContent = editModalNameInput.value;
-   profileDescription.textContent = editModalDescriptionInput.value;
-   closeModal();
-};
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+  closeModal();
+}
 
-
-profileEditButton.addEventListener('click', openModal);
-profileCloseButton.addEventListener('click', closeModal);
-editFormElement.addEventListener('submit', handleEditFormSubmit);
-
+profileEditButton.addEventListener("click", openModal);
+profileCloseButton.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
 
 for (let i = 0; i < initialCards.length; i++) {
-const cardElement = getCardElement(initialCards[i]);
-cardsList.prepend(cardElement);
-};
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
